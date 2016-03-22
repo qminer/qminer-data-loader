4474a0bd925e99ad83bcb5943beaacc44e5e88a1
Jan Rupnik <jan.rupnik@ijs.si>
2016-03-22 12:20:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+exports.loadSentimentDataset = function (store, fileName) {
+    fileName = fileName == undefined ? __dirname + '/sentiment.ldjson' : fileName;
+    store.loadJson(fileName);
+}
+
+exports.loadRecommendationDataset = function (store, fileName) {
+    fileName = fileName == undefined ? __dirname + '/visits_small.ldjson' : fileName;
+    store.loadJson(fileName);
+}
+
+exports.loadMoviesDataset = function (store, fileName) {
+    fileName = fileName == undefined ? __dirname + '/movies.ldjson' : fileName;
+    store.loadJson(fileName);
+}
+
+
+exports.loadForexDataset = function (store, fileName) {
+    fileName = fileName == undefined ? __dirname + '/forex.ldjson' : fileName;
+    store.loadJson(fileName);
+}
+
+exports.getSentimentDatasetFile = function (fileName) {
+    return __dirname + '/' + fileName;
+}
